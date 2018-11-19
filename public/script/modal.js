37ec0ccebf1d7344ee1e8ff996485036474c67ad
gwontaeyong <taaii6569@naver.com>
2018-11-19 12:46:37 +0900

    delete unused file
    delete css, script, html files

--- public/script/modal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  // Get the modal
-  var modal = document.getElementById('myModal');
-
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-  var btn2 = document.getElementById("myBtn2");
-});
